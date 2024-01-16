6b2dc2fb29f5b1019682b124f45f405a20e88620
VitorCruz78 <vitorluiz15012006@gmail.com>
2024-01-15 22:32:14 -0300

    Configuração de stories

--- node_modules/@ignite-ui/docs/vite.config.js
+++ node_modules/@ignite-ui/docs/vite.config.js
@@ -1,8 +1,6 @@
-<reference types="vite/client" />
-
-import React from "react"
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()]
+  plugins: [react()],
 })
